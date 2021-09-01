1de19c6b472aa06ff2d8d59ffd40d411cef07209
Matthew Bouc <boucgm@gmail.com>
2021-09-01 13:41:54 -0500

    starting GET locations/? search

--- server/routes/location.router.js
+++ server/routes/location.router.js
@@ -73,36 +73,38 @@ router.post('/', (res,req) => {
 
 
 
- // TESTING MERGING
 
 
-//     const query2 = `INSERT INTO companies_recyclables (company_id, recyclable_id) VALUES ($1, $2) return ID;`;
 
-//     for (const category in categories) {
-//             pool.query(query1, [companyId, recId])
-//             .then(dbRes => {
-//                 console.log(dbRes.rows[0].id);
 
-//             const newCompanyId = dbRes.rows[0].id;
-//             const query2 = `INSERT INTO service_areas VALUES ($3, $3);`;
 
-//             const query3 = 
 
-//             pool.query(query2, [newCompanyId, serviceAreas])
-//             .then(dbRes => {
-//                 res.send(dbRes.rows);
-//             })
-//             .catch(err => {
-//                 console.log('Unable to post new company', err);
-//                 res.sendStatus(500);
 
-//             })
-//         };
-//         .catch(err => {
-//             console.log('Unable to post new company', err);
-//             res.sendStatus(500);
-//         })
-//     }
-// })
+
+/**
+ * GET route for returning all companies
+ */
+router.get("/", (req, res) => {
+  const query = `SELECT
+  companies.*,
+  ARRAY_AGG(distinct service_areas.area) AS areas,
+  ARRAY_AGG(distinct recyclables.item) AS item
+FROM companies
+JOIN companies_recyclables ON companies_recyclables.company_id = companies.id
+JOIN service_areas ON service_areas.company_id = companies.id
+JOIN recyclables ON recyclables.id = companies_recyclables.recyclable_id
+GROUP BY companies.id;`;
+  pool
+    .query(query)
+    .then((dbRes) => {
+      res.send(dbRes.rows);
+    })
+    .catch((err) => {
+      console.log("Unable to return db data", err);
+      res.sendStatus(500);
+    });
+});
+
+
 
 module.exports = router;
