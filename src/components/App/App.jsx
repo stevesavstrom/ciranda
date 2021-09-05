a87131121e4ed08deb5215b57e85df30a85646b1
John Idso <john.d.idso@gmail.com>
2021-09-04 20:20:52 -0500

    tested and returning feedback data

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -39,17 +39,13 @@ function App() {
           <Route exact path="/search">
             <SearchPage />
           </Route>
-          {/* For protected routes, the view could show one of several things on the same route.
-            Visiting localhost:3000/user will show the UserPage if the user is logged in.
-            If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
-            Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-          <ProtectedRoute
-            // logged in shows UserPage else shows LoginPage
+
+          <Route
             exact
             path="/admin"
           >
             <AdminPage />
-          </ProtectedRoute>
+          </Route>
 
           <Route
             exact
