53627c13414a94371e1821993923d86dc377a53e
Steve Savstrom <steve.savstrom@gmail.com>
2021-09-06 12:28:51 -0500

    setup location-details.saga.js, imported locationDetails saga into root.reducer.js

---
+++ src/redux/sagas/location-details.saga.js
@@ -0,0 +1,18 @@
+import { put, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* locationDetailsSaga(){
+    yield takeEvery('GET_JOB_DETAILS', fetchLocationDetails);
+}
+
+function* fetchLocationDetails(action) {
+    try {
+        const response = yield axios.get(`/api/location/${action.payload}`);
+        console.log('Inside fetchLocationDetails: data is', response.data);
+        yield put({ type: 'SET_LOCATION_DETAILS', payload: response.data});
+    } catch (error) {
+        console.log('Inside fetchLocationDetails:  Error GETting location details client side', error);
+    }
+}
+
+export default locationDetailsSaga;
