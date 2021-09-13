54cbf25dad5588a960fddb7ec6f6b458ade3d76a
Matthew Bouc <boucgm@gmail.com>
2021-09-13 12:46:27 -0500

    login and register pages have onClick for presentation

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -38,11 +38,16 @@ function RegisterForm() {
   const paperStyle={padding:20, height:'50vh' ,width:400, margin:"20px auto"}
   const avatarStyle={backgroundColor: '#1bbd7e'}
 
+  const handleAutoFill = () => {
+    setUsername('Matt');
+    setEmail('matt@ciranda.com');
+    setPassword('123');
+  }
 
   return (
     <Grid>
         <Paper elevation={10} style ={paperStyle}>
-        <h2 align='center'>Register New Administrator</h2>
+        <h2 align='center' onClick={handleAutoFill}>Register New Administrator</h2>
     <TextField label='Username' placeholder='Enter Username' value={username} onChange={(event) => setUsername(event.target.value)} fullWidth required/>
     <TextField label='Email' type='email' placeholder='Enter Email' value={email} onChange={(event) => setEmail(event.target.value)} fullWidth required/>
     <TextField label='Password' type='password' placeholder='Enter password' value={password} onChange={(event) => setPassword(event.target.value)} fullWidth required/>
